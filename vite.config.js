91a9d2afb9b591bc7f18c3be90da4502beb84f67
pharouqy <pharouky@gmail.com>
2024-09-09 22:09:52 +0100

    Fix up the vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(
-  {
-    plugins: [react()],
-  },
-  {
-    server: {
-      proxy: {
-        "/api": {
-          target:
-            "https://script.google.com/macros/s/AKfycbxNu27V2Y2LuKUIQMK8lX1y0joB6YmG6hUwB1fNeVbgzEh22TcDGrOak03Fk3uBHmz-/exec",
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ""),
-        },
-      },
-    },
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
   }
-);
+})
+
