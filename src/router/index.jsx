c0ee86f82dd4b220d12494ac83fba2176e01ed8e
pharouqy <pharouky@gmail.com>
2024-09-21 16:42:02 +0100

    Another handle authentication solution

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,5 +1,4 @@
-import { Routes, Route, useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 import AuthCheck from "../components/authCheck";
 import Home from "../pages/home";
 import User from "../pages/profil";
@@ -15,45 +14,41 @@ const isAuthenticated = () => {
 };
 
 const AppRouter = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (isAuthenticated()) {
-      navigate("/home");
-    } else {
-      navigate("/");
-    }
-  }, [navigate]);
-
   return (
     <>
-      <Routes>
-        <Route
-          path="/home"
-          element={
-            <AuthCheck>
-              <Home />
-            </AuthCheck>
-          }
-        />
-        <Route
-          path="/user/:id"
-          element={
-            <AuthCheck>
-              <User />
-            </AuthCheck>
-          }
-        />
-        <Route path="/" element={<Login />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password/:token" element={<ResetPassword />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<Errors />} />
-      </Routes>
+      {isAuthenticated ? (
+        <Routes>
+          <Route
+            path="/home"
+            element={
+              <AuthCheck>
+                <Home />
+              </AuthCheck>
+            }
+          />
+          <Route
+            path="/user/:id"
+            element={
+              <AuthCheck>
+                <User />
+              </AuthCheck>
+            }
+          />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<Errors />} />
+        </Routes>
+      ) : (
+        <>
+          <Route path="/" element={<Login />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password/:token" element={<ResetPassword />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<Errors />} />
+        </>
+      )}
     </>
   );
 };
 
 export default AppRouter;
-
